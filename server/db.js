542c62ccd7649b37d40a94fd9a59868f5b2d9c02
exylus <thomy.erwan@gmail.com>
2024-09-28 22:45:08 +0200

    Updated db.js
    Changed it to a connection pool

--- server/db.js
+++ server/db.js
@@ -1,12 +1,15 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.HOST,
     port: process.env.PORT,
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
+    waitForConnections: true,
+    connectionLimit: 100,
+    queueLimit: 0,
 });
 
 db.connect((err) => {
