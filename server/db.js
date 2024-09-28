f70d0cb67f6fbfd603880adb90f0fc89e46fd936
exylus <thomy.erwan@gmail.com>
2024-09-28 15:38:28 +0200

    Created db.js
    added db.js to separate it from main code

---
+++ server/db.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const db = mysql.createConnection({
+    host: process.env.HOST,
+    port: process.env.PORT,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Database connection failed: ' + err.stack);
+        return;
+    }
+    console.log('Connected to the database.');
+});
+
+module.exports = db;
