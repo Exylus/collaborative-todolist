20945f61bbec0c66a711cda897c8578e2826631f
exylus <thomy.erwan@gmail.com>
2024-09-28 22:47:39 +0200

    Fixed db.js

--- server/db.js
+++ server/db.js
@@ -12,12 +12,4 @@ const db = mysql.createPool({
     queueLimit: 0,
 });
 
-db.connect((err) => {
-    if (err) {
-        console.error('Database connection failed: ' + err.stack);
-        return;
-    }
-    console.log('Connected to the database.');
-});
-
 module.exports = db;
